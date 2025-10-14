b405e99e97cb21961964fac094683666d4116f4b
Vedant Garud <vedusher@gmail.com>
2025-10-14 21:41:43 +0530

    added player movement logic

--- code/main.py
+++ code/main.py
@@ -37,7 +37,7 @@ class Game:
                     exit()
 
             # game logic
-
+            self.all_sprites.update()
             self.all_sprites.draw(self.display_surface )
             pygame.display.update()
                 
