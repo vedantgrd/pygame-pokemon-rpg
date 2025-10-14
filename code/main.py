98fc107b0158de4db4e0f055a75b61fdcfbb4a87
Vedant Garud <vedusher@gmail.com>
2025-10-14 19:16:31 +0530

    creating a basic window

---
+++ code/main.py
@@ -0,0 +1,22 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('pokemon-rpg')
+                
+    def run(self):
+        while True:
+            # event loop 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            # game logic
+            pygame.display.update()
+                
+if __name__ == '__main__':
+    game = Game()
+    game.run()
