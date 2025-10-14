cb8a73e240b04ddc2b060ecfcd16d1bdf2c39b38
kryyo1441 <aayushnair321@gmail.com>
2025-10-14 22:02:14 +0530

    fixed issue regarding missing player

--- code/entites.py
+++ code/entites.py
@@ -4,7 +4,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.Surface((100, 100))
-        self.image.fill("Red")
+        self.image.fill("red")
         self.rect = self.image.get_frect(center=pos)
 
         self.direction = vector()
