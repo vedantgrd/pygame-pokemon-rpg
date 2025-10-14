f14bd659c154130b375e8f105e2203a5608dc926
kryyo1441 <aayushnair321@gmail.com>
2025-10-14 20:57:04 +0530

    displayed top left of the map using the new sprite function by creating a group to show it.

--- code/main.py
+++ code/main.py
@@ -1,12 +1,16 @@
 from settings import *
 from pytmx.util_pygame import load_pygame 
 from os.path import join
+from sprites import Sprite
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('pokemon-rpg')
+
+        #groups
+        self.all_sprites = pygame.sprite.Group() # this group will contain all the sprites, well all the visible ones atleast :)
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
@@ -16,7 +20,7 @@ class Game:
     
     def setup(self, tmx_map, player_start_pos):
         for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            print(x * TILE_SIZE,y * TILE_SIZE, surf)
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
 
 
     def run(self):
@@ -28,6 +32,8 @@ class Game:
                     exit()
 
             # game logic
+
+            self.all_sprites.draw(self.display_surface )
             pygame.display.update()
                 
 if __name__ == '__main__':
