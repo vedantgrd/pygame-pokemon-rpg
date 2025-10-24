361f4576b9ee76079a45e60659394b6d52efca1b
Vedant Garud <vedusher@gmail.com>
2025-10-24 19:03:35 +0530

    storing surfaces

--- code/support.py
+++ code/support.py
@@ -9,3 +9,12 @@ def import_image(*path, alpha = True, format = 'png'):
 	surf = pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
 	return surf
 
+
+def import_folder(*path):
+	frames = []
+	for folder_path, sub_folders, image_names in walk(join(*path)):
+		for image_name in sorted(image_names, key = lambda name: int(name.split('.')[0])):
+			full_path = join(folder_path, image_name)
+			surf = pygame.image.load(full_path).convert_alpha()
+			frames.append(surf)
+	return frames
