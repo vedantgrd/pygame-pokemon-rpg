cf8dc9ffaf933e85812ab235d06a38a572a5808d
Vedant Garud <vedusher@gmail.com>
2025-10-24 19:37:07 +0530

    added basic water animation

--- code/sprites.py
+++ code/sprites.py
@@ -8,4 +8,13 @@ class Sprite(pygame.sprite.Sprite):
 
 class AnimatedSprite(Sprite):
     def __init__(self, pos, frames, groups):
-        super().__init__(pos, frames[0], groups)
+        self.frame_index, self.frames = 0, frames
+        super().__init__(pos, frames[self.frame_index], groups)
+
+
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[int(self.frame_index  % len(self.frames))]
+
+    def update(self, dt):
+        self.animate(dt)
