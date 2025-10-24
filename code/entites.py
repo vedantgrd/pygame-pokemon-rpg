18110eec2dfd87bc596581c22fb223964a14fd83
Vedant Garud <vedusher@gmail.com>
2025-10-24 21:21:36 +0530

    replaced 'red rectangle' as the actual player (character) image

--- code/entites.py
+++ code/entites.py
@@ -4,13 +4,18 @@ class Entity(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups):
         super().__init__(groups)
 
+     # graphics
+        self.frame_index, self.frames = 0, frames
+
+         # sprite setup
+        self.image = self.frames['down'][self.frame_index]
+        self.rect = self.image.get_frect(center = pos)
+    
+
 class Player(Entity):
     def __init__(self, pos, frames, groups):
         super().__init__(pos, frames, groups)
-        self.image = pygame.Surface((100, 100))
-        self.image.fill("red")
-        self.rect = self.image.get_frect(center=pos)
-
+     
         self.direction = vector()
 
     def input(self):
